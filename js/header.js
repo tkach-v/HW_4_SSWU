7c1480bdf77abfe8fd6b4f7802fc145a1aef47af
Volodymyr Tkach <vovantkac5@gmail.com>
2023-07-24 22:37:26 +0300

    create footer, header-top

---
+++ js/header.js
@@ -0,0 +1,37 @@
+function changeProgressBarWidth() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.querySelector('.header__top-progress-bar').style.width = scrolled + "%";
+}
+
+window.addEventListener('scroll', changeProgressBarWidth);
+
+document.addEventListener('click', event => {
+    const targetElement = event.target;
+
+    if (targetElement.closest('.header__toggler')) {
+        document.querySelector(".header__top").classList.toggle('header__top_open');
+    }
+
+    if (targetElement.closest('[data-goto]')) {
+        if (document.documentElement.classList.contains('header__top_open')) {
+            document.documentElement.classList.remove();
+        }
+
+        const goTo = targetElement.closest('[data-goto]').dataset.goto;
+        const goToElement = document.querySelector(goTo);
+        const headerHeight = document.querySelector('.header__top').offsetHeight;
+
+        if (goToElement) {
+            window.scrollTo({
+                top: goToElement.offsetTop - headerHeight,
+                behavior: "smooth"
+            });
+        }
+        event.preventDefault();
+    }
+});
+
+
+
